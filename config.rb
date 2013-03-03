0e741a5242f69bb9dad4414c60b535484c0a9e84
GrayGhostVisuals <grayghost@grayghostvisuals.com>
2013-03-03 15:46:56 -0500

    removed multiple logos and started using font squirrel to embed fonts

--- config.rb
+++ config.rb
@@ -1,2 +1,18 @@
 sass_dir = '_sass'
+images_dir = "images"
 relative_assets = true
+
+#Projects Font Directory
+fonts_dir = "fonts"
+
+# Add support for repeating linear gradients
+# https://github.com/chriseppstein/compass/issues/401
+Compass::BrowserSupport.add_support("repeating-linear-gradient", "webkit", "moz", "ms")
+
+# Development
+#output_style = :expanded
+#environment = :development
+
+# Production
+output_style = :compressed
+environment = :production
